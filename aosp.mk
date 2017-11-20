40fceddc29dc2ceb47e0c15e4ab3ebfbd8416b0f
Xaif <huzaifaamirka46@gmail.com>
2017-11-20 16:40:32 +0530

    Update and rename xpe.mk to aosp.mk

--- aosp.mk
+++ aosp.mk
@@ -3,8 +3,8 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
-# Inherit some common XPE stuff.
-$(call inherit-product, vendor/xperience/config/common_full_phone.mk)
+# Inherit some common XOS stuff.
+$(call inherit-product, vendor/xos/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
